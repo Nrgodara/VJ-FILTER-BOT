19141dfdf2b2d746523e5d3d98c6962a16b03c66
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-10-03 21:38:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -34,9 +34,11 @@ SPELL_CHECK = {}
 @Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
     if message.chat.id != SUPPORT_CHAT_ID:
+        content = message.text
         settings = await get_settings(message.chat.id)
         chatid = message.chat.id 
         user_id = message.from_user.id if message.from_user else 0
+        if content.startswith("/") or content.startswith("#"): return
         if settings['fsub'] != None:
             try:
                 btn = await pub_is_subscribed(client, message, settings['fsub'])
