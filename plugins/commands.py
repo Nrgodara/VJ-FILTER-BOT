a30bfb72272e7ec9847abd72f717768290ea2ca6
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-10-05 16:36:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,7 +22,10 @@ join_db = JoinReqs
 
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
-    await message.react(emoji=random.choice(REACTIONS))
+    try:
+        await message.react(emoji=random.choice(REACTIONS))
+    except:
+        await message.react(emoji="⚡️")
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[
             InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
