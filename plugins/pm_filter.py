18ec3f4845d0cf74e34328a0451c221427f2fd18
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-10-05 16:37:17 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -33,6 +33,11 @@ SPELL_CHECK = {}
 
 @Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
+    try:
+        await message.react(emoji=random.choice(REACTIONS))
+    except:
+        await message.react(emoji="⚡️")
+    
     if message.chat.id != SUPPORT_CHAT_ID:
         content = message.text
         settings = await get_settings(message.chat.id)
